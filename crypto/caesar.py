a66024ffd27efb8aa577bd441e6f0665fd052314
Unknown <llee.design@outlook.com>
2017-08-24 21:43:21 -0500

    start encrypt

--- crypto/caesar.py
+++ crypto/caesar.py
@@ -9,8 +9,15 @@ def rotate_character(char, rot):
     else:
         num = alphabet_position(char)   #index of char 
         num = num + rot                 #index of char moved over rot
-        alpha = alphabet[num % 26]      #num coverted to letter using alphabet list [ ]
+        alpha = alphabet[num % 26]      #num coverted to letter using alphabet 
         return alpha
 
+#print(rotate_character('a',13))
 
-print(rotate_character('a',13))
+def encrypt(text, rot):
+    output = ''
+    for a in text:
+        output += rotate_character(a, rot)
+    return output 
+
+print(encrypt("LaunchCode", 13))
